e9c03959e97d8ccee3f06665f30fc295e07810c2
dmitrytakhtamir <72991969+dmitrytakhtamir@users.noreply.github.com>
2021-06-08 16:12:37 +0300

    Update settings.py

--- nevsky/settings.py
+++ nevsky/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/assets')]
+    os.path.join(BASE_DIR, 'build/assets'),]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
